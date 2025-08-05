c7d37c61e47ce5d52651686fd1491efbc758643d
Shekina-Tautho <supagwapashikyna@gmail.com>
2025-08-05 20:48:10 +0800

    Append createdTask to tasks, issue: overwrites previous task

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
+//tasks object 
+let tasks = {
 
-function AddTask() {
-    const tasks = {
-        id: 123,
-        description: "damn"
-    };
-    console.log(tasks);
-}
+};
 
 //create object
 function createTask(id, task) {
@@ -20,13 +16,15 @@ function createTask(id, task) {
 function appendTask() {
     let input = document.getElementById('task').value; // get input value
     let uuid = self.crypto.randomUUID(); //generate random uuid
-    console.log(uuid);
-    let var1 = createTask(uuid, input); //asign input to property
-    console.log(var1);
-    
+    //console.log(uuid);
+    let createdTask = createTask(uuid, input); //asign input to property
+    //console.log(createdTask);
+    tasks = {...createdTask};
+    console.log(tasks);
 }
 
 
-//create a function that creates objects, then append the created object to the tasks object
+//goal1: create a function that creates objects, then append the created object to the tasks object
+//create a pre-made object (will hold the task created) that will not change values upon refresh
 //figure out how to dynamically create variable names and assign objects to thos so
 //we can append it to takss object
