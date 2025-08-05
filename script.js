2c02641b2247ec109af15c6247de9201bf2fba83
Shekina-Tautho <supagwapashikyna@gmail.com>
2025-08-05 23:46:28 +0800

    fix overwrite issue

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
 //tasks object 
 let tasks = {
-
+    
 };
 
 //create object
-function createTask(id, task) {
+function createTask(task) {
     console.log("task created");
     return {
-        Id: id,
         task: task
     };
 }
 
-
-function appendTask() {
+function appendTask(event) {
+    event.preventDefault();
+    let UUID = self.crypto.randomUUID();
     let input = document.getElementById('task').value; // get input value
-    let uuid = self.crypto.randomUUID(); //generate random uuid
-    //console.log(uuid);
-    let createdTask = createTask(uuid, input); //asign input to property
-    //console.log(createdTask);
-    tasks = {...createdTask};
+    let createdTask = createTask(input); //asign input to property
+    tasks[UUID] = {...createdTask}; //append input as value of UUID
     console.log(tasks);
 }
 
 
-//goal1: create a function that creates objects, then append the created object to the tasks object
-//create a pre-made object (will hold the task created) that will not change values upon refresh
-//figure out how to dynamically create variable names and assign objects to thos so
-//we can append it to takss object
