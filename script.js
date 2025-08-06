bfb1ebc47b52786814e3db4336609b06c074344e
Shekina-Tautho <supagwapashikyna@gmail.com>
2025-08-06 21:17:03 +0800

    save to localstorage

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let tasks = {
 
 //create object
 function createTask(task) {
-    console.log("task created");
     return {
         task: task
     };
@@ -17,8 +16,23 @@ function appendTask(event) {
     let input = document.getElementById('task').value; // get input value
     let createdTask = createTask(input); //asign input to property
     tasks[UUID] = {...createdTask}; //append input as value of UUID
-    console.log(tasks);
+    save();
 }
 
-//try to save to localstorage next
+function save() {
+    console.log("saved to local storage");
+    let tempObj = JSON.stringify(tasks);
+    localStorage.setItem("tasks", tempObj) //save to local storage
+}
 
+window.onload = function () {
+    let stored = localStorage.getItem("tasks");
+    if (stored) {
+        tasks = JSON.parse(stored);
+        console.log(tasks);
+    } else {
+        console.log("not tasks found");
+    }
+}
+
+//next: display tasks in ui
