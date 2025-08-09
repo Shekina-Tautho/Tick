ef1257afd99ad96306259ac5faa3d70efe340b20
Shekina-Tautho <supagwapashikyna@gmail.com>
2025-08-09 13:08:25 +0800

    Add delete logic

--- script.js
+++ script.js
@@ -39,7 +39,10 @@ function appendContainer(UUID, taskValue) {
     let createdContainer = {
         task: taskValue,
         rowClass: "row",
-        gridClass: "col-md-4 bg-info"
+        gridClass: "col-md-4 bg-info",
+        deleteText: "Delete",
+        deleteClass: "btn btn-danger",
+        uuid: UUID
     };
     containers[UUID] = createdContainer;
 
@@ -52,7 +55,6 @@ function saveContainers() {
     localStorage.setItem("containers", JSON.stringify(containers));
 }
 
-
 function renderContainer(c) {
     let rowDiv = document.createElement('div');
     rowDiv.className = c.rowClass;
@@ -61,8 +63,27 @@ function renderContainer(c) {
     gridDiv.className = c.gridClass;
     gridDiv.textContent = c.task;
 
+    let delBtn = document.createElement('button');
+    delBtn.className = c.deleteClass;
+    delBtn.textContent = c.deleteText;
+    gridDiv.appendChild(delBtn);
+    delBtn.dataset.uuid = c.uuid;
+
     rowDiv.appendChild(gridDiv);
     document.body.appendChild(rowDiv);
+
+    delBtn.addEventListener('click', handleDelete);
+
+}
+
+function handleDelete(event) {
+    let uuid = event.currentTarget.dataset.uuid;
+    delete tasks[uuid];
+    delete containers[uuid];
+    saveTasks();
+    saveContainers();
+    event.target.closest('div').remove();
+    console.log("delete test");
 }
 
 
@@ -85,3 +106,6 @@ window.onload = function () {
 
 
 //next: delete button per card
+//first: render delete button inside card
+//second: add onclick event to delbtn, 
+// delete key associated to container & task from local storage
